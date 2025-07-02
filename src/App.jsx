959a7f495b5d27d86df57c09af4841efdbdc4113
jeffersonbalde <jeffersonbalde8@gmail.com>
2025-07-02 10:15:02 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import "./styles/style.scss";
-import Login from "./pages/Login/Login";
-import { ToastContainer, toast } from "react-toastify";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import RequireAuth from "./context/RequireAuth";
-import { default as ShowServices } from "./components/admin/Services/Show";
-import { default as CreateServices } from "./components/admin/Services/Create";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+  import Home from "./pages/Home/Home";
+  import About from "./pages/About/About";
+  import "./styles/style.scss";
+  import Login from "./pages/Login/Login";
+  import { ToastContainer, toast } from "react-toastify";
+  import Dashboard from "./pages/Dashboard/Dashboard";
+  import RequireAuth from "./context/RequireAuth";
+  import { default as ShowServices } from "./components/admin/Services/Show";
+  import { default as CreateServices } from "./components/admin/Services/Create";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admin/login" element={<Login />} />
-          <Route
-            path="/admin/dashboard"
-            element={
-              <RequireAuth>
-                <Dashboard />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/admin/services"
-            element={
-              <RequireAuth>
-                <ShowServices />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/admin/services/create"
-            element={
-              <RequireAuth>
-                <CreateServices />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer position="top-center" />
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admin/login" element={<Login />} />
+            <Route
+              path="/admin/dashboard"
+              element={
+                <RequireAuth>
+                  <Dashboard />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/admin/services"
+              element={
+                <RequireAuth>
+                  <ShowServices />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/admin/services/create"
+              element={
+                <RequireAuth>
+                  <CreateServices />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </HashRouter>
+        <ToastContainer position="top-center" />
+      </>
+    );
+  }
 
-export default App;
+  export default App;
